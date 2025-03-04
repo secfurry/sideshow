a840c031710eb8ced15a529935ab75e027c30167
secfurry <dijit@secfurry.com>
2025-03-04 16:33:04 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,175 @@
+# SideShow
+
+eInk display Badge based on the Pinorami InkyFrame.
+
+## How does it work?
+
+SideShow uses the built-in SD Card reader combined with the PCF RTC to run as
+a super low power badge by displaying images on the eInk display.
+
+## States
+
+### Automatic
+
+With the default configration, the device will "wake-up" every 15 minutes automatically
+and update the display. When not processing anything, the device will attempt to
+turn itself off to save power when using the JST battery connector on the InkyFrame
+board.
+
+The display update process follows the following routes:
+
+- Increase the "current display" count (held in PCF memory) by `1`.
+  - If the "current display" count is `>= 127`, reset it to `0`.
+- Open the "/backgrounds" (changable by configuration) directory on the SD Card.
+  - Get the current file count in this directory
+  - Choose a random image from this directory and write it's parsed contents to
+    the eInk display.
+- Open the "/badges" (changable by configuration) directory on the SD Card,
+  - Iterate through the files in the directory until one of the conditions are met.
+    - File count equals the "current display" count.
+    - File is the last file in the directory.
+  - If the file was the last entry, the "current display" count will be set to `127`.
+  - Write the selected image's parsed contents to the eInk display.
+- Update the display
+
+The dual background and "badge" images work on the concept that TGA images allow
+for transparency, which is supported by our TGA parser. Transparent pixels will
+__not__ be drawn and can allow for the previously drawn pixels to show through.
+
+This selection process allows for randomized backgrounds for every "badge" displayed.
+
+NOTE: SideShow will only read TGA-type image files. You can use `imagmagick` to
+convert them easily using `convert src.jpg dst.tga`
+
+It's also recommended that the images are the size of the eInk display (640x400 for InkyFrame4)
+as SideShow will draw them at (0, 0) directly.
+
+### Buttons
+
+The button configuration can be changed but supports the following button actions:
+
+- __Lock__: Prevent the current "badge" from being changed automatically. This will
+  change the background but not the "badge". When this action is used, the top
+  LEDs (Action and Network) will display the new Lock state.
+  - Action On, Network Off: Lock is Disabled.
+  - Action Off, Network On: Lock is Enabled.
+  It's a toggle button to switch the Lock state.
+- __Random__: Select a random "badge" and background and display it. This will
+  override the Lock value and disable it. The "current display" count will be set
+  to the current "badge" position.
+- __Previous__: Select the previous "badge" and display it. The "current display"
+  count will be set to the current "badge" position (`-1`). This button __does not__
+  override the Lock, if set. If the current "badge" is the first entry (`0`), the
+  "current display" value will be set to `127`, which wraps around the selection.
+- __Next__: Select the next "badge" and display it. The "current display"
+  count will be set to the current "badge" position (`+1`). This button __does not__
+  override the Lock, if set. If the current "badge" is the last entry, the
+  "current display" value will be set to `0`, which wraps around the selection.
+- __Custom__: TODO.
+
+The default button configuration is:
+
+- A: Nothing
+- B: Lock
+- C: Random
+- D: Previous
+- E: Next
+
+When a button is pressed, it's LED will light up indicating the pressed selection.
+
+### Errors
+
+If SideShow encounters an error, it will switch to an error state and will require it
+to be power-cycled to clear it.
+
+Error states are indicated by the Activity and Network LEDs flashing back-and-forth
+every second with one of the button LEDs enabled.
+
+The button LED indicates the type of error that occurred.
+
+- A: __ByteFail__: Operations on the PCF stored byte failed.
+- B: __LoadFail__: Image processing operation failed. This is usually due to a badly
+     formatted, corrupted file or non-TGA image.
+- C: __WakeFail__: Operations on the PCF wake alarm and interrupts failed.
+- D: __InvalidPins__: Setup for the eInk display was not correct. Usually this error
+     is due to a configuration/code error.
+- E: __InvalidRoot__: Operations on the SD Card (non-image related) failed. This is
+     usually due to an error with the SD Card or it's formatting. Sometimes it
+     will be a fluke issue, but may require re-formatting the SD Card if the error
+     occurs multiple times in a row.
+
+## Configuration
+
+The configuration is fairly simple. Any changes made are in `sideshow.rs` in the
+following code block:
+
+```rust
+// =================== [   Configuration   ] ===================
+/// Time in milliseconds) to wait for a button check. Only
+/// takes affect when NOT on battery power.
+const SLEEP_STEP: u32 = 50u32;
+/// Time (in seconds) to wake up and change the current
+/// badge and/or background.
+const SLEEP_TIME: u32 = 15u32 * 60u32;
+
+/// Directory name in the SD Card root to get the badge
+/// images from.
+const DIR_BADGES: &str = "/badges";
+/// Directory name in the SD Card root to get the background
+/// images from.
+const DIR_BACKGROUNDS: &str = "/backgrounds";
+
+/// Action to return when the 'A' button is pressed.
+const BUTTON_A: Action = Action::None;
+/// Action to return when the 'B' button is pressed.
+const BUTTON_B: Action = Action::Lock;
+/// Action to return when the 'C' button is pressed.
+const BUTTON_C: Action = Action::Rand;
+/// Action to return when the 'D' button is pressed.
+const BUTTON_D: Action = Action::Prev;
+/// Action to return when the 'E' button is pressed.
+const BUTTON_E: Action = Action::Next;
+// =================== [ Configuration End ] ===================
+```
+
+Each name is self-explanatory and any changes require a re-compilation and upload to
+the device to take affect.
+
+## Setup
+
+To start using SideShow, you'll need to compile this repository first.
+
+The cargo configuration should contain all the values needed to compile in the
+`thumbv6m-none-eabi` target format (for the RP2040 processor). You'll need to make
+sure you have the `flip-link` linker installed before compiling. To do this, use
+the command `cargo install flip-link` to install it.
+
+If a Pico debug probe is avaliable (or you can setup one
+[see here](https://mcuoneclipse.com/2022/09/17/picoprobe-using-the-raspberry-pi-pico-as-debug-probe/)),
+installing `probe-rs` using `cargo install probe-rs-tools` will allow for direct
+flashing of the firmware when the probe is connected to the SideShow device when
+`cargo run` is called.
+
+If you don't have a debug probe avaliable, you can convert the compiled ELF into
+a U2F file using `elf2uf2-rs` and can be loaded using the Bootloader mode of the
+Pico (hold down the "Boot" button when plugging the device in to expose the flash
+storage) by copying the generated file over to flash. _Make sure to comment out_
+_the first two lines of `.cargo/config.toml` to avoid any compilation errors._
+
+### Case
+
+Included in the `case` directory are STL files that can be used to 3d-print a
+nice case to contain the device with:
+
+- USB-C connector port
+- Simple switch (for on/off)
+- LiPo Battery Pack
+- AdaFruit PowerBoost 1000
+
+## InkyFrame Devices
+
+The current code configuration is support for the InkyFrame 4. With a couple of
+code and configuration changes, it can work on different devices or larger screen
+sizes.
+
+See the [InkyFrame](https://github.com/secfurry/inky-frame) repository for compatibility.
---
+++ src/sideshow.rs
@@ -0,0 +1,391 @@
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#![no_implicit_prelude]
+
+extern crate core;
+extern crate inky_frame;
+extern crate rpsp;
+
+use core::convert::Into;
+use core::iter::{IntoIterator, Iterator};
+use core::option::Option::{None, Some};
+use core::result::Result::{self, Ok};
+use core::unreachable;
+
+use inky_frame::InkyBoard;
+use inky_frame::frame::tga::TgaParser;
+use inky_frame::frame::{Inky, InkyPins, InkyRotation};
+use inky_frame::fs::{BlockDevice, Mode, Volume};
+use inky_frame::hw::{Button, Buttons, Leds};
+use rpsp::MayFail;
+use rpsp::rand::Rand;
+
+use crate::out;
+
+// =================== [   Configuration   ] ===================
+/// Time in milliseconds) to wait for a button check. Only
+/// takes affect when NOT on battery power.
+const SLEEP_STEP: u32 = 50u32;
+/// Time (in seconds) to wake up and change the current
+/// badge and/or background.
+const SLEEP_TIME: u32 = 15u32 * 60u32;
+
+/// Directory name in the SD Card root to get the badge
+/// images from.
+const DIR_BADGES: &str = "/badges";
+/// Directory name in the SD Card root to get the background
+/// images from.
+const DIR_BACKGROUNDS: &str = "/backgrounds";
+
+/// Action to return when the 'A' button is pressed.
+const BUTTON_A: Action = Action::None;
+/// Action to return when the 'B' button is pressed.
+const BUTTON_B: Action = Action::Lock;
+/// Action to return when the 'C' button is pressed.
+const BUTTON_C: Action = Action::Rand;
+/// Action to return when the 'D' button is pressed.
+const BUTTON_D: Action = Action::Prev;
+/// Action to return when the 'E' button is pressed.
+const BUTTON_E: Action = Action::Next;
+// =================== [ Configuration End ] ===================
+
+pub enum SideError {
+    ByteFail,
+    LoadFail,
+    WakeFail,
+    InvalidPins,
+    InvalidRoot,
+}
+
+pub struct SideShow<'a, const B: usize, const W: u16, const H: u16, D: BlockDevice> {
+    inky:  Inky<'a, B, W, H>,
+    root:  &'a Volume<'a, D>,
+    rand:  Rand,
+    board: &'a InkyBoard<'a>,
+}
+
+pub type SideShowInky4<'a, D> = SideShow<'a, 128_000, 640u16, 400u16, D>;
+
+enum Action {
+    None,
+    Next,
+    Prev,
+    Rand,
+    Wake,
+    Lock,
+    // TODO(sf): Custom Actions
+    // Custom,
+}
+
+impl<'a, D: BlockDevice> SideShowInky4<'a, D> {
+    #[inline(always)]
+    pub fn new(b: &'a InkyBoard<'a>, root: &'a Volume<'a, D>, r: impl Into<InkyRotation>) -> Result<SideShowInky4<'a, D>, SideError> {
+        SideShowInky4::create(b, root, InkyPins::inky_frame4(), r)
+    }
+}
+impl<'a, const B: usize, const W: u16, const H: u16, D: BlockDevice> SideShow<'a, B, W, H, D> {
+    #[inline]
+    pub fn create(b: &'a InkyBoard<'a>, root: &'a Volume<'a, D>, pins: InkyPins, r: impl Into<InkyRotation>) -> Result<SideShow<'a, B, W, H, D>, SideError> {
+        let mut i = Inky::new(b, b.spi_bus(), pins).map_err(|_| SideError::InvalidPins)?;
+        i.set_rotation(r.into());
+        Ok(SideShow {
+            root,
+            inky: i,
+            rand: Rand::new(),
+            board: b,
+        })
+    }
+
+    pub fn run(&mut self) -> MayFail<SideError> {
+        let (l, b) = (self.board.leds(), self.board.buttons());
+        loop {
+            //
+            out!("loop enter, time: {}", self.board.pcf().now().unwrap());
+            //
+            l.all_off();
+            // Read PFC register, fallback to 0 if it fails.
+            let s = self.board.pcf().get_byte().unwrap_or(0);
+            //
+            out!("PFC byte_read={s}, starting switch..");
+            //
+            let n = self.switch(s, l, b)?;
+            //
+            out!("Switch result n={n}, setting PFC byte..");
+            //
+            l.all_off();
+            self.board.pcf().set_byte(n).map_err(|_| SideError::ByteFail)?;
+            self.board.sleep(2_500);
+            //
+            out!("Setting PFC rtc_wake time..");
+            //
+            let w = self.board.set_rtc_wake(SLEEP_TIME).map_err(|_| SideError::WakeFail)?;
+            unsafe { self.board.power_off() };
+            // Everything after this means we're on AC power.
+            //
+            out!("Board is NOT on battery power, running manual sleep..");
+            //
+            self.sleep(b, w);
+            // Disable alarm and reset RTC state.
+            let _ = self.board.pcf().alarm_clear_state();
+            let _ = self.board.pcf().alarm_disable();
+        }
+    }
+
+    fn sleep(&mut self, b: &mut Buttons, w: u32) {
+        let mut v = w;
+        while v > 0 {
+            self.board.sleep(SLEEP_STEP);
+            // Watch for button presses.
+            if b.read_pressed() {
+                break;
+            }
+            v = v.saturating_sub(SLEEP_STEP);
+        }
+        // If nothing happens, it's an RTC wake-up. Indicate it to allow wake.
+        // This will only happen when connected to AC power.
+        if !b.button_any() {
+            b.set(Button::RTC);
+        }
+    }
+    #[inline(always)]
+    fn background(&mut self) -> Result<(), SideError> {
+        self.random_set_image(DIR_BACKGROUNDS)?;
+        Ok(())
+    }
+    fn badge(&mut self, act: Action, cur: u8) -> Result<u8, SideError> {
+        match &act {
+            Action::None => return Ok(cur), // Just in case.
+            // Random: Override the Badge lock and set a random one. Set this
+            //         new badge position as the index, without the lock on.
+            Action::Rand => return Ok(self.random_set_image(DIR_BADGES)? as u8),
+            // Wake: Don't change the badge selected if the lock is on, if it's
+            //       off, act like Next.
+            // Next: Don't change the badge selected if the lock is on, if it's
+            //       off, select the next badge, wrapping if over the badge count.
+            // Prev: Don't change the badge selected if the lock is on, if it's
+            //       off, select the last badge, resetting to the max if zero.
+            _ => (),
+        }
+        let n = cur & 0x7F;
+        let k = match act {
+            _ if cur & 0x80 != 0 => n,                     // All stay the same when the lock is enabled.
+            Action::Next | Action::Wake if n >= 0x7F => 0, // Wrap and reset.
+            Action::Next | Action::Wake => n + 1,          // Advance the count.
+            Action::Prev if n == 0 => 0x7F,                // Reset to the max.
+            Action::Prev => n - 1,                         // Reduce the count.
+            _ => unreachable!(),                           // Can't happen.
+        };
+        let i = {
+            let d = self.root.dir_open(DIR_BADGES).map_err(|_| SideError::LoadFail)?;
+            // Use the 'peekable' iter so we can check if the number goes out of
+            // bounds so we can fix the max.
+            let mut v = d
+                .list()
+                .map_err(|_| SideError::LoadFail)?
+                .into_iter()
+                .filter(|e| e.as_ref().is_ok_and(|v| v.is_file()))
+                .peekable();
+            let mut i = 0u8;
+            // Use a loop so we can pull back to make sure we catch the end value.
+            let mut f = loop {
+                let e = v.next().ok_or(SideError::LoadFail)?.map_err(|_| SideError::LoadFail)?;
+                // If the next one is None, that means we're at the end.
+                if i == k || v.peek().is_none() || i >= 0x7F {
+                    break e;
+                }
+                i = i.saturating_add(1);
+            }
+            .into_file(&self.root, Mode::READ)
+            .map_err(|_| SideError::LoadFail)?;
+            self.inky
+                .set_with(|x| x.set_image(0, 0, TgaParser::new(&mut f)?))
+                .map_err(|_| SideError::LoadFail)?;
+            // If 'i' is less than 'k', that means we hit the limit of the reads
+            // so we should set the value to the max for a reset.
+            if i < k || v.peek().is_none() { 0x7F } else { i }
+        };
+        Ok((cur & 0x80) | i)
+    }
+    fn random_set_image(&mut self, dir: &str) -> Result<usize, SideError> {
+        let d = self.root.dir_open(dir).map_err(|_| SideError::LoadFail)?;
+        let mut l = d.list().map_err(|_| SideError::LoadFail)?;
+        let n = l.into_iter_mut().filter(|e| e.as_ref().is_ok_and(|v| v.is_file())).count();
+        l.reset(&d).map_err(|_| SideError::LoadFail)?;
+        let i = self.rand.rand_u32n(n as u32) as usize;
+        let e = l
+            .into_iter()
+            .filter(|e| e.as_ref().is_ok_and(|v| v.is_file()))
+            .nth(i)
+            .map(|v| v.ok())
+            .flatten();
+        let mut f = match e {
+            Some(v) => v,
+            None => return Ok(i),
+        }
+        .into_file(&self.root, Mode::READ)
+        .map_err(|_| SideError::LoadFail)?;
+        self.inky
+            .set_with(|x| x.set_image(0, 0, TgaParser::new(&mut f)?))
+            .map_err(|_| SideError::LoadFail)?;
+        Ok(i)
+    }
+    #[inline]
+    fn switch(&mut self, sel: u8, l: &Leds, b: &mut Buttons) -> Result<u8, SideError> {
+        // Check if any button was pressed.
+        let a = match b.pressed() {
+            Button::ButtonA => {
+                l.a.on();
+                BUTTON_A
+            },
+            Button::ButtonB => {
+                l.b.on();
+                BUTTON_B
+            },
+            Button::ButtonC => {
+                l.c.on();
+                BUTTON_C
+            },
+            Button::ButtonD => {
+                l.d.on();
+                BUTTON_D
+            },
+            Button::ButtonE => {
+                l.e.on();
+                BUTTON_E
+            },
+            Button::None => Action::None,
+            Button::RTC | Button::External => Action::Wake,
+        };
+        // Signal online.
+        l.activity.on();
+        match a {
+            Action::None => return Ok(sel),
+            Action::Lock => {
+                let v = if sel & 0x80 != 0 {
+                    // Indicate lock is off.
+                    l.network.off();
+                    l.activity.on();
+                    sel & 0x7F
+                } else {
+                    // Indicate lock is on.
+                    l.network.on();
+                    l.activity.off();
+                    sel | 0x80
+                };
+                // Let the user know it was changed.
+                self.board.sleep(2_000);
+                return Ok(v);
+            },
+            Action::Rand if sel == 0 => {
+                l.all_on();
+                self.board.sleep(2_000);
+                return Ok(sel);
+            },
+            _ => (),
+        }
+        self.background()?;
+        l.network.on();
+        let r = self.badge(a, sel)?;
+        l.activity.off();
+        self.inky.update();
+        Ok(r)
+    }
+}
+
+#[inline]
+pub fn sideshow_error(e: SideError) -> ! {
+    let i = InkyBoard::get();
+    let l = i.leds();
+    l.all_off();
+    match e {
+        SideError::ByteFail => l.a.on(),
+        SideError::LoadFail => l.b.on(),
+        SideError::WakeFail => l.c.on(),
+        SideError::InvalidPins => l.d.on(),
+        SideError::InvalidRoot => l.e.on(),
+    }
+    loop {
+        i.sleep(1_500);
+        l.network.on();
+        l.activity.off();
+        i.sleep(1_500);
+        l.network.off();
+        l.activity.on();
+    }
+}
+#[inline(always)]
+pub fn sideshow_inky4(r: impl Into<InkyRotation>) -> ! {
+    let b = InkyBoard::get();
+    let d = b.sd_card();
+    let v = d.root().unwrap_or_else(|_| sideshow_error(SideError::InvalidRoot));
+    SideShowInky4::new(&b, &v, r)
+        .and_then(|mut x| x.run())
+        .unwrap_or_else(sideshow_error)
+}
+
+#[cfg(feature = "debug")]
+mod debug {
+    extern crate core;
+    extern crate rpsp;
+
+    use core::cell::UnsafeCell;
+    use core::fmt::{Arguments, Write};
+    use core::marker::Sync;
+    use core::option::Option::{self, None};
+
+    use rpsp::uart::Uart;
+
+    static DEBUG: DebugPort = DebugPort::empty();
+
+    struct DebugPort(UnsafeCell<Option<Uart>>);
+
+    impl DebugPort {
+        #[inline(always)]
+        const fn empty() -> DebugPort {
+            DebugPort(UnsafeCell::new(None))
+        }
+
+        #[inline(always)]
+        fn port(&self) -> &mut Uart {
+            unsafe { &mut *self.0.get() }.get_or_insert_with(|| rpsp::uart_debug())
+        }
+    }
+
+    unsafe impl Sync for DebugPort {}
+
+    #[inline(always)]
+    pub(super) fn output(args: Arguments<'_>) {
+        let _ = DEBUG.port().write_fmt(args);
+    }
+
+    #[macro_export]
+    macro_rules! out {
+        ($($arg:tt)*) => {{
+            debug::output(core::format_args!($($arg)*));
+        }};
+    }
+}
+#[cfg(not(feature = "debug"))]
+mod debug {
+    #[macro_export]
+    macro_rules! out {
+        ($($arg:tt)*) => {{}};
+    }
+}
